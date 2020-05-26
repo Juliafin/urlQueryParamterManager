c33d40632465eafcf3b5e90f662f6875aeb88d5f
Julia Finarovsky <juliafinarovsky@gmail.com>
2020-05-25 22:54:05 -0400

    Implemented context api

--- url-query-parameter-manager/src/App.js
+++ url-query-parameter-manager/src/App.js
@@ -1,166 +1,63 @@
 import React from "react";
+import { Container, Button } from "@material-ui/core";
+import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
 import "./App.css";
 import { Configuration } from "./components/Configuration";
 import { ParamsDisplay } from "./components/ParamsDisplay";
-import { getCurrentUrl, parseUrlParams } from "./utils/parseUrl";
-import { setUrl } from "./utils/setUrl";
-import { createQueryString } from "./utils/createQueryString";
-import {
-  getConfiguration,
-  saveConfiguration,
-  getKeyHistory,
-  setKeyHistory,
-} from "./utils/configurationUtils";
-import cloneDeep from "lodash.clonedeep";
-import { Container, Button } from "@material-ui/core";
-import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
+import { ManagerContext } from "./store/URLManagerContext";
 
-const defaultFields = { key: "name", value: "value", id: 1 };
+const App = () => {
+  const {
+    currentConfiguration,
+    configurations,
+    handlers,
+    keyHistory,
+    queryFields,
+    tabId,
+    url,
+  } = React.useContext(ManagerContext);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "",
-      tabId: "",
-      queryFields: [defaultFields],
-      currentConfiguration: "",
-      configurations: {},
-      keyHistory: {},
-    };
-  }
+  const isConfigurationButtonDisabled = !Boolean(currentConfiguration);
+  return (
+    <div className="App">
+      <h1>Url Query Parameter Manager</h1>
+      <p>Current url: {url}</p>
+      <Configuration
+        configurationChangeHandler={handlers.onConfigurationChangeHandler}
+        configurationOptions={Object.keys(configurations)}
+      />
+      <ParamsDisplay
+        queryFieldOnChangeHandler={handlers.queryFieldOnChangeHandler}
+        queryFields={queryFields}
+        queryFieldOnDeleteHandler={handlers.queryFieldOnDeleteHandler}
+        keyHistory={keyHistory}
+      />
+      <Container
+        className="flex addContainer"
+        onClick={handlers.queryFieldOnAddHandler}
+      >
+        <AddCircleOutlineRoundedIcon className="icon addCircle" />
+      </Container>
 
-  async componentDidMount() {
-    const { currentUrl: url, tabId } = await getCurrentUrl();
-    const { configurations } = await getConfiguration();
-    const { keyHistory } = await getKeyHistory();
-    console.log("key history in mount", keyHistory);
-    const queryFields = parseUrlParams(url);
-    this.setState({
-      configurations,
-      keyHistory,
-      queryFields: queryFields.length ? queryFields : this.state.queryFields,
-      tabId,
-      url,
-    });
-  }
-
-  queryFieldOnDeleteHandler = (event) => {
-    const { id } = event.currentTarget;
-    let index = parseInt(id.split("-")[1], 10) - 1;
-    let newQueryFields = cloneDeep(this.state.queryFields);
-    newQueryFields.splice(index, 1);
-
-    newQueryFields = newQueryFields.map((field, index) => {
-      return { ...field, id: index + 1 };
-    });
-
-    this.setState({ queryFields: newQueryFields });
-  };
-
-  queryFieldOnChangeHandler = (event, fieldValue) => {
-    const { id, value } = event.target;
-    let newQueryFields = cloneDeep(this.state.queryFields);
-    const idArray = id.split("-");
-    const index = parseInt(idArray[idArray.length - 1], 10) - 1;
-    if (id.startsWith("key")) {
-      newQueryFields[index].key = value;
-    } else if (id.startsWith("value")) {
-      newQueryFields[index].value = value;
-    }
-
-    this.setState({ queryFields: newQueryFields });
-  };
-
-  setUrlHandler = (event) => {
-    const queryString = createQueryString(this.state.queryFields);
-    const url = this.state.url.replace(/(.*)(\?.*)/, "$1");
-    setUrl(queryString, url);
-  };
-
-  queryFieldOnAddHandler = (event) => {
-    let fieldsToAdd = [];
-    if (!this.state.queryFields || !this.state.queryFields.length) {
-      fieldsToAdd.push(defaultFields);
-    } else {
-      const lastIndex = this.state.queryFields.length - 1;
-      fieldsToAdd = [
-        ...cloneDeep(this.state.queryFields),
-        { ...defaultFields, ...{ id: this.state.queryFields[lastIndex].id } },
-      ];
-    }
-    fieldsToAdd = fieldsToAdd.map((field, index) => {
-      return { ...field, id: index + 1 };
-    });
-    this.setState({ queryFields: fieldsToAdd });
-  };
-
-  onConfigurationChangeHandler = (event) => {
-    const configurationName = event.target.value;
-    const configuration =
-      this.state.configurations[configurationName] || this.state.queryFields;
-    this.setState(
-      { currentConfiguration: configurationName, queryFields: configuration },
-      () => {}
-    );
-  };
-
-  saveConfigurationHandler = async (event) => {
-    const configurations = await saveConfiguration(
-      this.state.currentConfiguration,
-      this.state.queryFields
-    );
-
-    const keys = await setKeyHistory(this.state.queryFields);
-    console.log(keys, "KEYS IN SAVE CONFIGURATION");
-    this.setState({ configurations, keyHistory: keys }, () => {});
-  };
-
-  render() {
-    const isConfigurationButtonDisabled = !Boolean(
-      this.state.currentConfiguration
-    );
-    return (
-      <div className="App">
-        <h1>Url Query Parameter Manager</h1>
-        <p>Current url: {this.state.url}</p>
-        <Configuration
-          configurationChangeHandler={this.onConfigurationChangeHandler}
-          configurationOptions={Object.keys(this.state.configurations)}
-        />
-        <ParamsDisplay
-          queryFieldOnChangeHandler={this.queryFieldOnChangeHandler}
-          queryFields={this.state.queryFields}
-          queryFieldOnDeleteHandler={this.queryFieldOnDeleteHandler}
-          keyHistory={this.state.keyHistory}
-        />
-        <Container
-          className="flex addContainer"
-          onClick={this.queryFieldOnAddHandler}
+      <Container className="configurationButtonContainer">
+        <Button
+          className="configurationButton"
+          onClick={handlers.setUrlHandler}
+          variant="outlined"
         >
-          <AddCircleOutlineRoundedIcon className="icon addCircle" />
-        </Container>
-
-        <Container className="configurationButtonContainer">
-          <Button
-            className="configurationButton"
-            onClick={this.setUrlHandler}
-            variant="outlined"
-          >
-            Set Url
-          </Button>
-          <Button
-            className="configurationButton"
-            onClick={this.saveConfigurationHandler}
-            disabled={isConfigurationButtonDisabled}
-            variant="outlined"
-          >
-            Save Configuration
-          </Button>
-        </Container>
-      </div>
-    );
-  }
-}
+          Set Url
+        </Button>
+        <Button
+          className="configurationButton"
+          onClick={handlers.saveConfigurationHandler}
+          disabled={isConfigurationButtonDisabled}
+          variant="outlined"
+        >
+          Save Configuration
+        </Button>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
---
+++ url-query-parameter-manager/src/store/URLManagerContext.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { getCurrentUrl, parseUrlParams } from "../utils/parseUrl";
+import { setUrl } from "../utils/setUrl";
+import { createQueryString } from "../utils/createQueryString";
+import {
+  getConfiguration,
+  saveConfiguration,
+  getKeyHistory,
+  setKeyHistory,
+} from "../utils/configurationUtils";
+import cloneDeep from "lodash.clonedeep";
+
+export const ManagerContext = React.createContext();
+
+const defaultFields = { key: "name", value: "value", id: 1 };
+
+export class UrlManagerContextProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      tabId: "",
+      queryFields: [defaultFields],
+      currentConfiguration: "",
+      configurations: {},
+      keyHistory: {},
+    };
+  }
+
+  async componentDidMount() {
+    const { currentUrl: url, tabId } = await getCurrentUrl();
+    const { configurations } = await getConfiguration();
+    const { keyHistory } = await getKeyHistory();
+    const queryFields = parseUrlParams(url);
+    this.setState({
+      configurations,
+      keyHistory,
+      queryFields: queryFields.length ? queryFields : this.state.queryFields,
+      tabId,
+      url,
+    });
+  }
+
+  queryFieldOnDeleteHandler = (event) => {
+    const { id } = event.currentTarget;
+    let index = parseInt(id.split("-")[1], 10) - 1;
+    let newQueryFields = cloneDeep(this.state.queryFields);
+    newQueryFields.splice(index, 1);
+
+    newQueryFields = newQueryFields.map((field, index) => {
+      return { ...field, id: index + 1 };
+    });
+
+    this.setState({ queryFields: newQueryFields });
+  };
+
+  queryFieldOnChangeHandler = (event, fieldValue) => {
+    const { id, value } = event.target;
+    let newQueryFields = cloneDeep(this.state.queryFields);
+    const idArray = id.split("-");
+    const index = parseInt(idArray[idArray.length - 1], 10) - 1;
+    if (id.startsWith("key")) {
+      newQueryFields[index].key = value;
+    } else if (id.startsWith("value")) {
+      newQueryFields[index].value = value;
+    }
+
+    this.setState({ queryFields: newQueryFields });
+  };
+
+  setUrlHandler = (event) => {
+    const queryString = createQueryString(this.state.queryFields);
+    const url = this.state.url.replace(/(.*)(\?.*)/, "$1");
+    setUrl(queryString, url);
+  };
+
+  queryFieldOnAddHandler = (event) => {
+    let fieldsToAdd = [];
+    if (!this.state.queryFields || !this.state.queryFields.length) {
+      fieldsToAdd.push(defaultFields);
+    } else {
+      const lastIndex = this.state.queryFields.length - 1;
+      fieldsToAdd = [
+        ...cloneDeep(this.state.queryFields),
+        { ...defaultFields, ...{ id: this.state.queryFields[lastIndex].id } },
+      ];
+    }
+    fieldsToAdd = fieldsToAdd.map((field, index) => {
+      return { ...field, id: index + 1 };
+    });
+    this.setState({ queryFields: fieldsToAdd });
+  };
+
+  onConfigurationChangeHandler = (event) => {
+    const configurationName = event.target.value;
+    const configuration =
+      this.state.configurations[configurationName] || this.state.queryFields;
+    this.setState(
+      { currentConfiguration: configurationName, queryFields: configuration },
+      () => {}
+    );
+  };
+
+  saveConfigurationHandler = async (event) => {
+    const configurations = await saveConfiguration(
+      this.state.currentConfiguration,
+      this.state.queryFields
+    );
+
+    const keys = await setKeyHistory(this.state.queryFields);
+    this.setState({ configurations, keyHistory: keys }, () => {});
+  };
+
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      
+      <ManagerContext.Provider
+        value={{
+          handlers: {
+            queryFieldOnDeleteHandler: this.queryFieldOnDeleteHandler,
+            queryFieldOnChangeHandler: this.queryFieldOnChangeHandler,
+            setUrlHandler: this.setUrlHandler,
+            queryFieldOnAddHandler: this.queryFieldOnAddHandler,
+            onConfigurationChangeHandler: this.onConfigurationChangeHandler,
+            saveConfigurationHandler: this.saveConfigurationHandler
+          },
+          configurations: this.state.configurations,
+          currentConfiguration: this.state.currentConfiguration,
+          keyHistory: this.state.keyHistory,
+          queryFields: this.state.queryFields,
+          tabId: this.state.tabId,
+          url: this.state.url
+        }}
+      >
+        {children}
+      </ManagerContext.Provider>
+    )
+  }
+
+}
+
+UrlManagerContextProvider.contextType = ManagerContext;
